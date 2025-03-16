78315654f6258f3be90661c89b46d112deb8ddc3
Mampionona33 <ramamps33@gmai.com>
2025-03-16 21:15:43 +0300

    fix responsive desing

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -10,23 +10,29 @@ export interface MenuItem {
 
 const MenuCard = ({ item }: { item: MenuItem }) => {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-slate-50 shadow-md justify-between w-min min-h-[35rem] ">
+    <div className="flex flex-col rounded-lg bg-slate-50 shadow-md w-full sm:w-72 md:w-80 min-h-[35rem]">
+      {/* Image */}
       <img
         src={item.image}
         alt={item.title}
-        className="rounded-t-lg min-w-80 "
+        className="rounded-t-lg w-full h-48 object-cover"
       />
 
-      <div className="flex flex-col gap-2 p-8 rounded-b-lg space-y-6 ">
+      {/* Contenu */}
+      <div className="flex flex-col gap-4 p-6 flex-grow">
+        {/* Titre & Prix */}
         <div className="flex flex-row justify-between">
           <h2 className="text-xl font-semibold">{item.title}</h2>
           <p className="text-lg font-semibold text-orange-500">${item.price}</p>
         </div>
-        <p className="text-lg">{item.description}</p>
 
+        {/* Description */}
+        <p className="text-lg flex-grow">{item.description}</p>
+
+        {/* Bouton */}
         <Link
           to={item.link}
-          className="flex flex-row justify-between hover:text-orange-500"
+          className="flex flex-row justify-between items-center hover:text-orange-500 mt-auto"
         >
           <p>Order for delivery</p>
           <span className="material-symbols-outlined">shopping_basket</span>
